f7462c665929b90e2e79603ab0945a265267839f
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-15 20:52:34 -0700

    Get paragraph description

--- parser.rb
+++ parser.rb
@@ -2,21 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-# doc = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/index.php?menu=1442'))
-# all_links = doc.css('.frontLink')
-# entity_names = all_links.inject([]) { |names, link| names << link.text.strip }
-# entity_links = all_links.inject([]) { |links, link| links << link['href'] }
-# entities = entity_names.zip(entity_links)
-# p "entity names: #{entity_names.count}"
-# p entity_names
-# p "entity links: #{entity_links.count}"
-# p entity_links
-# p 'entities'
-# p entities
-# pry.binding
-
-#unaction67_long
-
 def doc(html)
   Nokogiri::HTML(open('html'))
 end
@@ -37,18 +22,51 @@ def entities(doc)
   entities = entity_names(doc).zip(entity_links(doc))
 end
 
-def paragraph_nums(page)
-  paragraphs = page.xpath('//b[contains(text(), "In support of Rio+20 outcome paragraph")]')
-  paragraph_nums = []
-  paragraphs.each do |p|
-    paragraph = p.text.strip.gsub(/[\r\n-]/,'').strip
-    paragraph_nums << paragraph_num = paragraph[/(?<=paragraph ).*/].to_i
-  end
-  paragraph_nums.uniq.sort
+def paragraphs(page)
+  page.xpath('//b[contains(text(), "In support of Rio+20 outcome paragraph")]')
 end
 
-page = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/'+entity_links[0]+'#un'))
-p paragraph_nums(page)
+def all_paragraph_nums(page)
+  paragraphs(page).inject([]) do |paragraph_nums, paragraph|
+    formatted_paragraph = paragraph.text.strip.gsub(/[\r\n]/,'').strip
+    paragraph_num = formatted_paragraph[/(?<=paragraph )\d*/].to_i
+    paragraph_nums << paragraph_num
+  end
+end
+
+def paragraph_nums(page)
+  all_paragraph_nums(page).uniq.sort
+end
+
+def page(link)
+  Nokogiri::HTML(open('http://sustainabledevelopment.un.org/'+link+'#un'))
+end
+
+def print_entity_paragraph_nums(doc)
+  entities(doc).each do |name, link|
+    p name
+    p paragraph_nums(page(link))
+  end
+end
+
+doc = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/index.php?menu=1442'))
+# print_entity_paragraph_nums(doc)
+
+### Driver Code ###
+doc = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/index.php?menu=1442'))
+page = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/'+entity_links(doc)[0]+'#un'))
+# p paragraph_nums(page) == [24, 38, 47, 54, 62, 66, 68, 78, 84, 91, 93, 100, 106, 108, 109, 133, 137, 154, 156, 179, 182, 185, 186, 243, 244, 248, 251, 280]
+# p paragraphs(page)
+paragraph = paragraphs(page).first.parent.text.strip
+# pry.binding
+# paragraph = paragraphs(page).first.parent.text
+formatted_paragraph = paragraph.gsub(/[\r\n]/,'').strip
+p paragraph_desc = formatted_paragraph[/(?<=-  ).*/]
+# paragraphs(page).inject([]) do |paragraph_nums, paragraph|
+#   formatted_paragraph = paragraph.text.strip.gsub(/[\r\n]/,'').strip
+#   paragraph_num = formatted_paragraph[/(?<=-  ).*/]
+#   paragraph_nums << paragraph_num
+# end
 
 
 
