eb61357f776dd1b1d1966ffd5908d4a6a9388224
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-16 22:38:00 -0700

    correct #doc to make html to work

--- parser.rb
+++ parser.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'pry'
 
 def doc(html)
-  Nokogiri::HTML(open('html'))
+  Nokogiri::HTML(open(html))
 end
 
 def all_links(doc)
@@ -31,9 +31,14 @@ def paragraph_num(paragraph)
   formatted_paragraph[/(?<=paragraph )\d*/].to_i
 end
 
+def paragraph_desc(paragraph)
+  formatted_paragraph = paragraph.parent.text.strip.gsub(/[\r\n]/,'').strip
+  paragraph_desc = formatted_paragraph[/(?<=-  ).*/]
+end
+
 def all_paragraph_nums(entity_page)
   paragraphs(entity_page).inject([]) do |paragraph_nums, paragraph|
-    paragraph_nums << paragraph_num(paragraph)
+    paragraph_nums << [paragraph_num(paragraph), paragraph_desc(paragraph)]
   end
 end
 
@@ -47,18 +52,24 @@ end
 
 def print_entity_paragraph_nums(doc)
   entities(doc).each do |name, link|
-    p name
-    p paragraph_nums(entity_page(link))
+    puts name
+    puts paragraph_nums(entity_page(link))
+    puts ""
   end
 end
 
-def paragraph_desc(paragraph)
-  formatted_paragraph = paragraph.parent.text.strip.gsub(/[\r\n]/,'').strip
-  paragraph_desc = formatted_paragraph[/(?<=-  ).*/]
+def all_entity_paragraph_nums(doc)
+  all_nums = []
+  entities(doc).each do |_, link|
+    all_nums << paragraph_nums(entity_page(link))
+  end
+  all_nums.flatten(1).uniq.sort
 end
 
-doc = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/index.php?menu=1442'))
+doc = doc('http://sustainabledevelopment.un.org/index.php?menu=1442')
 # print_entity_paragraph_nums(doc)
+p all_entity_paragraph_nums(doc).count
+p all_entity_paragraph_nums(doc)
 
 ### Driver Code ###
 doc = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/index.php?menu=1442'))
@@ -66,16 +77,10 @@ entity_page = Nokogiri::HTML(open('http://sustainabledevelopment.un.org/'+entity
 # p paragraph_nums(entity_page) == [24, 38, 47, 54, 62, 66, 68, 78, 84, 91, 93, 100, 106, 108, 109, 133, 137, 154, 156, 179, 182, 185, 186, 243, 244, 248, 251, 280]
 # p paragraph_desc(entity_page) == "Establish a universal intergovernmental high level political forum (HLPF), building on the strengths, experiences, resources and inclusive participation modalities of the Commission on Sustainable Development, and subsequently replacing the Commission."
 # p paragraphs(entity_page)
+
 paragraph = paragraphs(entity_page).first
-# pry.binding
-# paragraph = paragraphs(entity_page).first.parent.text
-# formatted_paragraph = paragraph.gsub(/[\r\n]/,'').strip
-p paragraph_desc(paragraph)
-# paragraphs(entity_page).inject([]) do |paragraph_nums, paragraph|
-#   formatted_paragraph = paragraph.text.strip.gsub(/[\r\n]/,'').strip
-#   paragraph_num = formatted_paragraph[/(?<=-  ).*/]
-#   paragraph_nums << paragraph_num
-# end
+# p paragraph_desc(paragraph)
+
 
 
 
